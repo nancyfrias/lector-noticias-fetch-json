916a45efb2ef4eb45a163758be6eaf3a186f08b0
Nancy Johana Frías Romero <nancyjohanafrias@gmail.com>
2026-05-22 01:13:04 -0600

    Agregar lector de noticias con Fetch y JSON

---
+++ README.md
@@ -0,0 +1,20 @@
+# Lector de Noticias
+
+Proyecto desarrollado en HTML, CSS y JavaScript para consumir noticias en tiempo real utilizando AJAX, Fetch y JSON.
+
+## Tecnologías utilizadas
+
+- HTML5
+- CSS3
+- JavaScript
+- Fetch API
+- JSON
+- GNews API
+
+## Funcionalidades
+
+- Mostrar noticias en tiempo real
+- Actualizar noticias sin recargar página
+- Abrir noticia completa
+- Manejo de errores
+- Diseño responsivo
---
+++ script.js
@@ -0,0 +1,127 @@
+const contenedor=document.getElementById(
+"contenedorNoticias"
+);
+
+const boton=document.getElementById(
+"btnActualizar"
+);
+
+const API_KEY="d0f2c823d418a4b4bf904ca822cb2acd";
+
+async function cargarNoticias(){
+
+contenedor.innerHTML=
+"<p>Cargando noticias...</p>";
+
+try{
+
+const respuesta = await fetch(
+`https://gnews.io/api/v4/search?q=tecnologia&lang=es&country=mx&max=10&apikey=${API_KEY}`
+);
+
+if(!respuesta.ok){
+
+throw new Error(
+"Error al conectar API"
+);
+
+}
+
+const datos=await respuesta.json();
+
+if(
+!datos.articles ||
+datos.articles.length===0
+){
+
+contenedor.innerHTML=
+"<p>No hay noticias disponibles</p>";
+
+return;
+
+}
+
+mostrarNoticias(
+datos.articles
+);
+
+}
+
+catch(error){
+
+contenedor.innerHTML=`
+
+<p class="error">
+
+No fue posible cargar noticias
+
+</p>
+
+`;
+
+console.log(error);
+
+}
+
+}
+
+function mostrarNoticias(
+noticias
+){
+
+contenedor.innerHTML="";
+
+noticias.forEach(
+noticia=>{
+
+const tarjeta=
+document.createElement(
+"div"
+);
+
+tarjeta.classList.add(
+"noticia"
+);
+
+tarjeta.innerHTML=`
+
+<img src="${noticia.image}">
+
+<h2>
+
+${noticia.title}
+
+</h2>
+
+<p>
+
+${noticia.description || "Sin descripción"}
+
+</p>
+
+<a href="${noticia.url}"
+
+target="_blank">
+
+Leer noticia completa
+
+</a>
+
+`;
+
+contenedor.appendChild(
+tarjeta
+);
+
+}
+
+);
+
+}
+
+boton.addEventListener(
+"click",
+cargarNoticias
+);
+
+cargarNoticias();
